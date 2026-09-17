38462333ecab54fa9428d2f9fe269148d815a03a
Ange Umutoni <a.umutoni1@alustudent.com>
2026-09-17 12:07:05 +0200

    Adding functional Post component with preview and New badge logic

---
+++ mini-blog/src/components/Post.tsx
@@ -0,0 +1,26 @@
+import type { Post as PostType } from '../types/Post';
+import '../styles/Post.module.css';
+
+interface PostProps {
+  post: PostType;
+}
+
+function Post({ post }: PostProps) {
+  const preview = post.content.split(' ').slice(0, 10).join(' ') + '...';
+
+  const isNew =
+    new Date().getTime() - new Date(post.datePosted).getTime() 
+    24 * 60 * 60 * 1000;
+
+  return (
+    <div className="post">
+      <h3>{post.title}</h3>
+      <p className="post-author">By {post.author}</p>
+      <p className="post-preview">{preview}</p>
+      <p className="post-date">{post.datePosted}</p>
+      {isNew && <span className="badge-new">New!</span>}
+    </div>
+  );
+}
+
+export default Post;
